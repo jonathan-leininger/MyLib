fb292910efd2d2c89232723a2f1704fc4dcec23c
Jonathan LEININGER <jonathan.leininger@open-groupe.com>
2017-08-28 22:22:22 +0200

    initial commit

---
+++ Example/MyLib/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyLib
+//
+//  Created by jonathan-leininger on 08/28/2017.
+//  Copyright (c) 2017 jonathan-leininger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
